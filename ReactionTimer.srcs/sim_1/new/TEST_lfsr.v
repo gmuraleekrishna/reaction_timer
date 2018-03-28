799704fea66e9f9b8c115844eea0637f518273ce
Harish Kp <kperatchi@gmail.com>
2018-03-28 13:54:43 +0530

    TEST_LFSR

--- ReactionTimer.srcs/sim_1/new/TEST_lfsr.v
+++ ReactionTimer.srcs/sim_1/new/TEST_lfsr.v
@@ -23,4 +23,36 @@
 module TEST_lfsr(
 
     );
+
+reg clk, reset;
+wire [32:0] random number;
+
+lfsr UUT(
+	.clk(clk),
+	.reset(reset),
+	.random_number(random_number)
+	);
+
+wire [32:0] temp;
+
+always begin
+	
+	#1
+	clk = ~clk;
+end
+
+initial begin
+	reset = 1;
+	random_number = 33'd8998957;
+
+	#5 
+	reset = 0;
+	temp = random_number;
+
+	#15
+	temp = random_number;
+
+	#25
+	temp = random_number;
+end
 endmodule
